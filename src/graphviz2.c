90922db72708573cbcc9faa98d92388211b17c5b
Wolfgang Pichler <pichlermeister@gmail.com>
2016-12-20 13:01:01 +0100

    added documentation added findPerson() added addPerson() added "EOF" command added mother,father, mgm relation handling added "list" feature

--- src/graphviz2.c
+++ src/graphviz2.c
@@ -12,17 +12,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+//not needed, as replaced by command2
 struct command {
 	char* action;
-	char* args;
+	char* args; //whole line
 };
+
+//specific command. arguments represent "cleaned" arguments
 struct command2 {
 	char* action;
 
-	int argc;
-	char** argv;
+	int argc; //real argument count (whitespaces already filtered..)
+	char** argv; //list of real arguments
 };
 
+//specific command for add operations
 struct AddCommand {
 	int valid; //0=false, 1=true
 	char* name1;
@@ -34,9 +38,27 @@ struct AddCommand {
 	char sex2; //f=female, m=male
 };
 
+typedef struct _person Person;
+struct _person {
+	char* name;
+	char sex;
+	Person* mother;
+	Person* father;
+};
+
+typedef struct _personvector PersonVector;
+
+struct _personvector {
+	int capacity;
+	int size;
+	Person* people;
+};
+
 /**
  * "re-formats" / joins names in add command
  * (a name can have whitespaces, those need to be "joined")
+ *
+ * this would be much simpler (and probably not even needed) if arguments where separated by doublequotes.. "
  */
 char** joinAddArguments(int argc, char* argv[], int *numtokens) {
 	if (argc < 5) {
@@ -74,9 +96,13 @@ char** joinAddArguments(int argc, char* argv[], int *numtokens) {
 		*numtokens = 5; //as we have found something and we have more than 5 arguments, we parse everything for 5 arguments...
 
 	for (int i = 0; i < rel_idx - 1; i++) {
+		if (i > 0)
+			name1_size += 1; //reserve for whitespace
 		name1_size += strlen(argv[i]);
 	}
 	for (int i = rel_idx + 1; i < argc; i++) {
+		if (i > 0)
+			name2_size += 1; //reserve for whitespace
 		name2_size += strlen(argv[i]);
 	}
 
@@ -84,6 +110,8 @@ char** joinAddArguments(int argc, char* argv[], int *numtokens) {
 	char* name1;
 	name1 = malloc(name1_size * sizeof(char)); //allocate memory for name1
 	for (int i = 0; i < rel_idx - 1; i++) {
+		if (i > 0)
+			strcat(name1, " "); //add whitespace
 		strcat(name1, argv[i]);
 	}
 	result[0] = name1;
@@ -94,6 +122,8 @@ char** joinAddArguments(int argc, char* argv[], int *numtokens) {
 	char* name2;
 	name2 = malloc(name2_size * sizeof(char)); //allocate memory for name2
 	for (int i = (rel_idx + 1); i < argc - 1; i++) {
+		if (i > (rel_idx + 1))
+			strcat(name2, " "); //add whitespace
 		strcat(name2, argv[i]);
 	}
 	result[3] = name2;
@@ -114,6 +144,7 @@ char parseSex(char* token) {
 	return ' ';
 }
 
+//converts list of arguments to "typed" operation for easier access..
 struct AddCommand parseAddCommand(int argc, char* argv[]) {
 	struct AddCommand cmd;
 
@@ -143,6 +174,7 @@ struct AddCommand parseAddCommand(int argc, char* argv[]) {
 	return cmd;
 }
 
+//read generic command from stdin
 struct command readCommand() {
 	struct command cmd;
 
@@ -165,16 +197,7 @@ struct command readCommand() {
 	cmd.action = action;
 	cmd.args = args;
 
-	free(line);
 	puts("");
-//		for (int i=0;i<read;i++){
-//			if (line[i]==' '){
-//				puts("");
-//			}else {
-//				putchar(line[i]);
-//			}
-//		}
-//free(line);
 
 	return cmd;
 
@@ -212,6 +235,7 @@ char** strsplit(const char* str, const char* delim, size_t* numtokens) {
 	free(s);
 	return tokens;
 }
+
 struct command2 readCommand2() {
 	struct command2 cmd;
 
@@ -228,7 +252,7 @@ struct command2 readCommand2() {
 	size_t numtokens;
 	char** tokens;
 	tokens = strsplit(line, " \n", &numtokens);
-//cmd.action=malloc(1*sizeof(char*));
+
 	cmd.action = (tokens[0]);
 
 	cmd.argc = numtokens - 1; //does this work?
@@ -255,41 +279,139 @@ int isValidAddCommand(struct AddCommand cmd) {
 	return 0;
 }
 
+//-1=not found,x=position
+Person* findPerson(PersonVector* vec, char* name, char sex) {
+	for (int i = 0; i < vec->size; i++) {
+		if ((strcmp(name, vec->people[i].name) == 0)
+				&& (sex == vec->people[i].sex)) {
+			return &vec->people[i];
+		}
+	}
+	return NULL;
+}
+//NULL in case of error
+//pointer to created person if success
+Person* addPerson(PersonVector* vec, char* name, char sex) {
+	if (vec->size == vec->capacity) {
+		return NULL;
+	}
+	Person* p;
+	p = malloc(sizeof(Person));
+	p->name = name;
+	p->sex = sex;
+	p->mother = NULL;
+	p->father = NULL;
+	vec->people[vec->size] = *p;
+	vec->size++;
+	return p;
+}
+
+PersonVector* createPeople(int capacity) {
+	PersonVector* result;
+
+	result = malloc(sizeof(PersonVector));
+	result->people = malloc(capacity * sizeof(Person));
+	result->capacity = capacity;
+	result->size = 0;
+
+	return result;
+}
+
 int main(int argc, char *argv[]) {
-//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	PersonVector* people;
+	people = createPeople(10);
 
 	struct command2 cmd;
-	cmd = readCommand2();
 
-	while (strcmp("quit", cmd.action) != 0) {
+	int exit = 0;
+	int unknown_count = 1; //unknown_count
 
-		if (strcmp("add", cmd.action) == 0) {
+	do {
+		cmd = readCommand2();
+
+		if (strcmp("EOF", cmd.action) == 0) {
+			exit = 1;
+		} else if (strcmp("quit", cmd.action) == 0) {
+			printf("good bye");
+			exit = 1;
+		} else if (strcmp("add", cmd.action) == 0) {
 			puts("add command recognized");
 
 			struct AddCommand add;
 			add = parseAddCommand(cmd.argc, cmd.argv);
 			if (isValidAddCommand(add) == 1) {
 				puts("add command is valid");
+				//char name[strlen(add.name1) + 1 + strlen("[f]")];
+				//strcpy(name, add.name1);
+				//strcpy(name, " [");
+				//strcpy(name, "" + add.sex1);
+				//strcpy(name, "]");
+
+				Person* p1 = findPerson(people, add.name1, add.sex1);
+				Person* p2 = findPerson(people, add.name2, add.sex2);
+				if (p1 == NULL) { //not found
+					p1 = addPerson(people, add.name1, add.sex1);
+				}
+				if (p2 == NULL) { //not found
+					p2 = addPerson(people, add.name2, add.sex2);
+				}
+				if (strcmp("father", add.relation) == 0) {
+					p2->father = p1;
+				} else if (strcmp("mother", add.relation) == 0) {
+					if (p2->mother == NULL) {
+						p2->mother = p1;
+					} else if (p2->mother != p1) {
+						printf("%s already has a %s as mother", p2->name,
+								p2->mother->name);
+					}
+				} else if (strcmp("mgm", add.relation) == 0) { //(mother-grandmother)
+					if (p2->mother == NULL) { //create unkown person
+						char name[100];
+						sprintf(name, "?%d", unknown_count);
+						unknown_count++;
+						Person* mother = addPerson(people, name, 'f');
+						p2->mother = mother;
+						mother->mother = p1;
+					} else { //relation already exists, check if that's true
+						if (p2->mother->mother == NULL) { //add relation mother's mother
+							p2->mother->mother = p1;
+						} else if (p2->mother->mother != p1) {
+							printf("[ERR] > invalid relation\n");
+						}
+					}
+				}
 			}
-			//printf("name1=%s,name2=%s\n", add.name1, add.name2);
-
-			//char name1[100],sex1[1],relation[100],name2[100],sex2[1];
-			//scanf("%s \\[%s\\] %s %s \\[%s\\]",name1,sex1,relation,name2,sex2);
-
-//			int i = 0;
-//			for (char c = line[i]; c != ' ' && c != '\n'; c = line[++i]) {
-//				if (c == ' ') {
-//					puts("");
-//				} else {
-//					putchar(c);
-//				}
-//			}
-
+		} else if (strcmp("list", cmd.action) == 0) {
+			if (cmd.argc > 0) {
+				puts("[ERR] Wrong usage - list.");
+			} else if (people->size == 0) {
+				puts("No entries available.");
+			} else {
+				//TODO: sort
+				for (int i = 0; i < people->size; i++) {
+					printf("%s [%c]\n", people->people[i].name,
+							people->people[i].sex);
+				}
+			}
+		} else if (strcmp("print", cmd.action) == 0) { //HELPER METHOD. NOT REQUIRED
+			if (people->size == 0) {
+				puts("No entries available.");
+			} else {
+				//TODO: sort
+				for (int i = 0; i < people->size; i++) {
+					Person p = people->people[i];
+					printf("%s [%c]\n", p.name, p.sex);
+					if (p.mother != NULL)
+						printf("  mother= %s [%c]\n", p.mother->name,
+								p.mother->sex);
+					if (p.father != NULL)
+						printf(" father=%s [%c]\n", p.father->name,
+								p.father->sex);
+				}
+			}
 		}
+	} while (exit == 0);
 
-		cmd = readCommand2();
-	}
-
-	printf("good bye");
 	return EXIT_SUCCESS;
 }
